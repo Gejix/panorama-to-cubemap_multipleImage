678ddee3bbf3cbf385dfb4d45aefde0bc02204b2
Gerald Juwah <geraldjuwah@gmail.com>
2024-03-23 16:00:55 +0000

    Update convert.js

--- convert.js
+++ convert.js
@@ -51,48 +51,48 @@ function mod(x, n) {
 //   };
 // }
 
-// performs a discrete convolution with a provided kernel
-// function kernelResample(read, write, filterSize, kernel) {
-//   const {width, height, data} = read;
-//   const readIndex = (x, y) => 4 * (y * width + x);
+performs a discrete convolution with a provided kernel
+function kernelResample(read, write, filterSize, kernel) {
+  const {width, height, data} = read;
+  const readIndex = (x, y) => 4 * (y * width + x);
 
-//   const twoFilterSize = 2*filterSize;
-//   const xMax = width - 1;
-//   const yMax = height - 1;
-//   const xKernel = new Array(4);
-//   const yKernel = new Array(4);
+  const twoFilterSize = 2*filterSize;
+  const xMax = width - 1;
+  const yMax = height - 1;
+  const xKernel = new Array(4);
+  const yKernel = new Array(4);
 
-//   return (xFrom, yFrom, to) => {
-//     const xl = Math.floor(xFrom);
-//     const yl = Math.floor(yFrom);
-//     const xStart = xl - filterSize + 1;
-//     const yStart = yl - filterSize + 1;
+  return (xFrom, yFrom, to) => {
+    const xl = Math.floor(xFrom);
+    const yl = Math.floor(yFrom);
+    const xStart = xl - filterSize + 1;
+    const yStart = yl - filterSize + 1;
 
-//     for (let i = 0; i < twoFilterSize; i++) {
-//       xKernel[i] = kernel(xFrom - (xStart + i));
-//       yKernel[i] = kernel(yFrom - (yStart + i));
-//     }
+    for (let i = 0; i < twoFilterSize; i++) {
+      xKernel[i] = kernel(xFrom - (xStart + i));
+      yKernel[i] = kernel(yFrom - (yStart + i));
+    }
 
-//     for (let channel = 0; channel < 3; channel++) {
-//       let q = 0;
+    for (let channel = 0; channel < 3; channel++) {
+      let q = 0;
 
-//       for (let i = 0; i < twoFilterSize; i++) {
-//         const y = yStart + i;
-//         const yClamped = clamp(y, 0, yMax);
-//         let p = 0;
-//         for (let j = 0; j < twoFilterSize; j++) {
-//           const x = xStart + j;
-//           const index = readIndex(clamp(x, 0, xMax), yClamped);
-//           p += data[index + channel] * xKernel[j];
+      for (let i = 0; i < twoFilterSize; i++) {
+        const y = yStart + i;
+        const yClamped = clamp(y, 0, yMax);
+        let p = 0;
+        for (let j = 0; j < twoFilterSize; j++) {
+          const x = xStart + j;
+          const index = readIndex(clamp(x, 0, xMax), yClamped);
+          p += data[index + channel] * xKernel[j];
 
-//         }
-//         q += p * yKernel[i];
-//       }
+        }
+        q += p * yKernel[i];
+      }
 
-//       write.data[to + channel] = Math.round(q);
-//     }
-//   };
-// }
+      write.data[to + channel] = Math.round(q);
+    }
+  };
+}
 
 // function copyPixelBicubic(read, write) {
 //   const b = -0.5;
