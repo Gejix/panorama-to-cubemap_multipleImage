c381f65ab746bfc828229416b3da9f660059e784
Gerald Juwah <geraldjuwah@gmail.com>
2024-03-23 14:14:51 +0000

    Update main.js

--- main.js
+++ main.js
@@ -37,7 +37,7 @@ document.addEventListener('DOMContentLoaded', () => {
                         imageData: imageData,
                         rotation:0,
                         face: face,
-                        interpolation: 'copyPixelLanczos',
+                        interpolation: 'linear',
                         originalName: file.name.split('.')[0],
                         operation: 'processFace',
                         maxWidth: 1024
