e7d400a81097f46240da07a3f6fe7a3a6af447d0
Gerald Juwah <geraldjuwah@gmail.com>
2024-03-24 00:41:53 +0000

    Update main.js

--- main.js
+++ main.js
@@ -133,7 +133,7 @@ function renderFace(data, face, position, folderName) {
 
     worker.onmessage = ({data: imageData}) => {
         const extension = settings.format.value;
-        getDataBlob(imageData, extension).then(blob =>             
+        getDataBlob(imageData, extension).then(blob =>
             face.addToZip(blob, extension, folderName);
 
             // Increment the count of completed tasks and check if all tasks are done
